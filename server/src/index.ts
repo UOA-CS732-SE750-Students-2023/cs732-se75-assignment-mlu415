f7e1fa5e3fef536ca16f82d9fa47b078f91df9aa
michelle <mlu415@aucklanduni.ac.nz>
2023-03-30 13:02:49 +1300

    add schema+env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,29 @@
+import {config} from "dotenv"
+config()
+
 import express, { Request, Response } from "express"
 import mongoose from "mongoose"
 
 import PostMessage from "./models/PostMessage"
 const app = express()
 
-const db = await mongoose.connect("mongodb+srv://michelle:test@atlascluster.oay4do7.mongodb.net/?retryWrites=true&w=majority")
+app.use(express.json()) //middleware
 
+const PORT  = 8080
 
-app.get("/", (req: Request,res: Response)=>{
-    res.send("Hello I am here")
+app.post("/posts", async (req: Request,res: Response)=>{
+    const newPost = new PostMessage({
+        title: req.body.title,
+        inventory: req.body.inventory,
+    });
+    const createdPost = await newPost.save();
+    res.json(createdPost)
 })
 
-app.listen(8080)
+
+mongoose.connect(process.env.ATLASURL ?? "").then(()=> {
+    console.log(`listening on port ${PORT}`)
+    app.listen(PORT)
+})
+
+
