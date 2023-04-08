f90c2f637d89469b83ce1d92679c05c8dd69629b
michelle <mlu415@aucklanduni.ac.nz>
2023-04-08 22:00:51 +1200

    setup signup

--- client/src/components/signuppage/signuppage.tsx
+++ client/src/components/signuppage/signuppage.tsx
@@ -1,60 +1,92 @@
-import styles from "./signuppage.module.scss";
-import { Avatar, Button, Card, TextField, Typography } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { AppDispatch } from "../../redux/store";
+import styles from "./signuppage.module.scss";
+import { auth } from "../../firebase/firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const Signuppage = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVerify, setPasswordVerify] = useState("");
+
+  const handleSignup = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (password !== passwordVerify) {
+      alert("Passwords do not match");
+      return;
+    }
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+      navigate("/");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  const handleGoogleSignup = async () => {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      await auth.signInWithPopup(provider);
+      navigate("/");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
 
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginContainerv2}>
         <h1>Create Your Account</h1>
+        <form onSubmit={handleSignup}>
+          <div className={styles.inputContainer}>
+            <Typography>EMAIL</Typography>
+            <TextField
+              className={styles.textfield}
+              placeholder="Enter your email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-        <div className={styles.inputContainer}>
-          <Typography>EMAIL</Typography>
-          <TextField
-            className={styles.textfield}
-            placeholder="Enter your email"
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
+          <div className={styles.inputContainer}>
+            <Typography>PASSWORD</Typography>
+            <TextField
+              className={styles.textfield}
+              placeholder="Password"
+              variant="outlined"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-        <div className={styles.inputContainer}>
-          <Typography>PASSWORD</Typography>
-          <TextField
-            className={styles.textfield}
-            placeholder="Password"
-            variant="outlined"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+          <div className={styles.inputContainer}>
+            <Typography>CONFIRM PASSWORD</Typography>
+            <TextField
+              className={styles.textfield}
+              placeholder="Confirm Password"
+              variant="outlined"
+              type="password"
+              value={passwordVerify}
+              onChange={(e) => setPasswordVerify(e.target.value)}
+            />
+          </div>
 
-        <div className={styles.inputContainer}>
-          <Typography>CONFIRM PASSWORD</Typography>
-          <TextField
-            className={styles.textfield}
-            placeholder="Confirm Password"
-            variant="outlined"
-            type="password"
-            value={passwordVerify}
-            onChange={(e) => setPasswordVerify(e.target.value)}
-          />
-        </div>
-
-        <button className={styles.loginBTN}>REGISTER</button>
+          <Button type="submit" className={styles.loginBTN}>
+            REGISTER
+          </Button>
+        </form>
 
         <span className={styles.or}>or</span>
-        <button className={styles.googleBTN}>
-          <i className="fa-brands fa-google"></i> Sign up with google
-        </button>
+
+        <Button onClick={handleGoogleSignup} className={styles.googleBTN}>
+          <i className="fa-brands fa-google"></i> Sign up with Google
+        </Button>
       </div>
     </div>
   );
--- client/src/firebase/firebase.ts
+++ client/src/firebase/firebase.ts
@@ -1,5 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-const config = {
+const firebaseConfig = {
     apiKey: "AIzaSyDE74kEJt5CCB-Y_Kl_4OB-p9qe3c6TtMo",
     authDomain: "fir-93d66.firebaseapp.com",
     projectId: "fir-93d66",
@@ -7,14 +10,10 @@ const config = {
     messagingSenderId: "545181922978",
     appId: "1:545181922978:web:d840c4b466a64746de09fb",
     measurementId: "G-DHG5545Z3E"
-  };
+};
 
-  export function getFirebaseConfig() {
-    if (!config || !config.apiKey) {
-      throw new Error('No Firebase configuration object provided.' + '\n' +
-      'Add your web app\'s configuration object to firebase-config.ts');
-    } else {
-      return config;
-    }
-  }    
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
   
+  export const auth = firebase.auth();
